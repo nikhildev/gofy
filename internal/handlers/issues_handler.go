b2a56d9bc3896549479e0dd1ad8bbcd073fc119c
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2024-07-28 12:09:22 +0300

    Tidying up

--- internal/handlers/issues_handler.go
+++ internal/handlers/issues_handler.go
@@ -1,41 +1,9 @@
 package handlers
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/nikhildev/gofy/internal/db/mongodb"
-	"github.com/nikhildev/gofy/internal/models"
-	"github.com/nikhildev/gofy/internal/services"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/labstack/echo/v4"
 )
 
-func GetIssuesCollection() *mongo.Collection {
-	client, _ := mongodb.NewDbStore(nil, "gofy")
-	return mongodb.GetCollection(*client, "issues")
-}
-
-func CreateIssueHandler(c *fiber.Ctx) error {
-	newIssue := &models.Issue{
-		Title:       "Test updated",
-		Description: "This is the issue description",
-		Reporter:    "Test",
-		Component:   "Test",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-		IsDeleted:   false,
-		Status:      models.IssueStatus("OPEN"),
-		Priority:    models.IssuePriority("LOW"),
-		Severity:    models.IssueSeverity("LOW"),
-	}
-
-	insertedId, err := services.CreateIssue(*newIssue)
-
-	if err != nil {
-		fmt.Printf("Error creating issue: %v", err)
-	}
-
-	return c.SendString(insertedId)
-
+func CreateIssueHandler(e echo.Context) error {
+	return e.String(200, "Create Issue Handler")
 }
--- internal/repositories/issue_repository.go
+++ internal/repositories/issue_repository.go
@@ -1,8 +1,6 @@
 package repositories
 
 import (
-	"fmt"
-
 	"github.com/nikhildev/gofy/internal/db/mongodb"
 	"github.com/nikhildev/gofy/internal/models"
 )
@@ -16,11 +14,8 @@ type issueRepository struct {
 }
 
 func (i *issueRepository) CreateIssue(issue *models.Issue) (string, error) {
-	fmt.Println("Creating issue")
-
 	coll := i.store.Db.Collection("issues")
 	res, err := coll.InsertOne(nil, issue)
-	fmt.Println(res)
 
 	if err != nil {
 		panic(err)
