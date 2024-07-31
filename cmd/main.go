f87fcc68aa6492a891193990c74d31903d86403b
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2024-07-31 19:38:28 +0300

    Tidying up

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,12 @@ import (
 
 func main() {
 
+	//This creates a simple http echo server and starts it
 	e := echo.New()
 
+	// For simplicity, we are registering all the routes in the main function
 	routes.RegisterRoutes(e)
 
+	// Start the server
 	e.Logger.Fatal(e.Start(":3000"))
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,10 +8,13 @@ import (
 
 // RegisterRoutes registers the routes for the application
 func RegisterRoutes(e *echo.Echo) {
+	// RequestID middleware generates a unique UUID for every request.
+	// The UUID can be retrieved using echo.HeaderXRequestID header.
+	// ex: requestId := c.Response().Header().Get(echo.HeaderXRequestID)
 	e.Use(middleware.RequestID())
 
+	// Creating groups help us to create subroutes without repetitiveness.
+	// Also, additional middleware can be applied to the group explicitly.
 	healthGroup := e.Group("/health")
 	handlers.HealthRoutes(healthGroup)
-
-	e.GET("/ping", handlers.PingHandler)
 }
