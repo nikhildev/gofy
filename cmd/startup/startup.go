7121bd0e062a0256cbebddb3533b7d7f64e05519
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2024-08-13 14:49:46 +0300

    Adding documentation for startup

--- cmd/startup/startup.go
+++ cmd/startup/startup.go
@@ -3,33 +3,39 @@ package startup
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/nikhildev/gofy/cmd/api"
 	"github.com/nikhildev/gofy/internal/db"
 	"github.com/spf13/cobra"
 	"go.mongodb.org/mongo-driver/mongo"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
+/*
+ * This is the startup command. This command is used to start the application.
+ * This command is a subcommand of the root command.
+ * The root command is the parent command and this command is the child command.
+ */
 func NewStartupCommand(store *db.Store) *cobra.Command {
-	// This is where we call the mongodb.NewStore() function to create a new instance of the Store struct. This function returns a pointer to the Store struct and an error. We are ignoring the error for now, but in a real-world application, you should handle it properly.
-
 	startupCommand := &cobra.Command{
 		Use:   "startup",
 		Short: "Startup",
 		Long:  `This command starts the application and introduces dependencies`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Starting the application")
+			fmt.Println("Starting the application ...")
 
 			// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
 			// Use a buffered channel to avoid missing signals as recommended for signal.Notify
-			quit := make(chan os.Signal)
+			quit := make(chan os.Signal, 1)
 
 			// Handle interrupt signal from the terminal
 			signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 
-			//This defer would catch the interrupts as well and disconnect the mongo client
+			// This defer would catch the interrupts as well and disconnect the mongo client.
+			// Because defer functions are executed outside the context of the main function,
+			// they need to have the client and context passed to them as dependencies.
 			defer func(client *mongo.Client, ctx context.Context) {
 				<-quit
 				cleanup(client, ctx)
@@ -39,10 +45,10 @@ func NewStartupCommand(store *db.Store) *cobra.Command {
 			api.StartApiServer()
 		},
 		PreRun: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Preparing to start the application")
+			fmt.Println("Preparing to start the application ...")
 		},
 		PostRun: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Application startup completed")
+			fmt.Println("Application startup completed!")
 		},
 	}
 
